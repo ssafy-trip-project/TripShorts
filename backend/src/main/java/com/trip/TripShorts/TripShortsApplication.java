efbc1961b18488fef5bf48982b7ce2a6747c56ba
wonjoon <seowj819@gmail.com>
2024-11-07 10:09:21 +0900

    backend init

---
+++ backend/src/main/java/com/trip/TripShorts/TripShortsApplication.java
@@ -0,0 +1,13 @@
+package com.trip.TripShorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripShortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripShortsApplication.class, args);
+	}
+
+}
